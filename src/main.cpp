73fb9a25ae7b325155d478bd1b92b9fd1cb47588
kilianp07 <kilianp07@icloud.com>
2025-02-05 09:16:42 +0100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,35 @@
 #include <Arduino.h>
-#include <Adafruit_AHTX0.h>
-#ifdef YNOV_ESP
-	#include <WiFi.h>
-#endif
+#include <I2S.h>
+#include <math.h>
+
+const int SAMPLE_RATE = 44100; 
+const int AMPLITUDE = 30000;   
+const float FREQUENCY = 1000.0; 
 
 void setup() {
-	// put your setup code here, to run once:
-	Serial.begin(115200);
-	pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+
+  if (!I2S.begin(I2S_PHILIPS_MODE, SAMPLE_RATE, 16)) {
+    Serial.println("Erreur : impossible de démarrer l'I2S.");
+    while (1);  
+  }
+
+  Serial.println("I2S démarré avec succès !");
 }
 
 void loop() {
-	// put your main code here, to run repeatedly:
-	digitalWrite(LED_PIN, HIGH);
-	delay(500);
-	digitalWrite(LED_PIN, LOW);
-	delay(500);
+  const int SAMPLES_PER_CYCLE = SAMPLE_RATE / FREQUENCY;  
+  int16_t sampleBuffer[SAMPLES_PER_CYCLE * 2];  
+  
+  for (int i = 0; i < SAMPLES_PER_CYCLE; i++) {
+    float theta = (2.0 * M_PI * i) / SAMPLES_PER_CYCLE;
+    int16_t sample = (int16_t)(AMPLITUDE * sin(theta));
+    
+    sampleBuffer[2 * i] = sample;      // Canal gauche
+    sampleBuffer[2 * i + 1] = sample;  // Canal droit
+  }
 
-#ifdef YNOV_ESP
-	Serial.println("I am ESP");
-#else
-	Serial.println("I am UNO");
-#endif
+  while (true) {
+    I2S.write((uint8_t*)sampleBuffer, sizeof(sampleBuffer));
+  }
 }
